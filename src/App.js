85f10d0915e1e60b437aeb24cfb68b7ae674564b
Wes <wespipkin@gmail.com>
2021-10-15 23:26:54 -0500

    uodates pkgs and removes uneeded imorts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 
 import { Nav } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.scss';
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-
-// import './index.css';
 import './FontAwesome';
 
 ReactDOM.render(<App />, document.getElementById('root'));
