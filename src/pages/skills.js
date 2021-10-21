ea0362c4b35403100acbb53af1f2805cbce162c4
Wes <wespipkin@gmail.com>
2021-10-21 14:11:39 -0500

    finishing touches on revamp and bug fix for project screen

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -5,8 +5,8 @@ import { SkillsCard } from '../components/SkillsCard';
 const Skills = () => {
   return (
     <>
-      <h3>Some of my skills:</h3>
-      <div className="row fade-in">
+      <h3 className="header">Some of my skills:</h3>
+      <div className="item-list fade-in">
         <SkillsCard />
         <div className="wrapper">
           <Cube />
